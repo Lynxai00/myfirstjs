df71b8c5a9e4cf07073de52295eee501f9bb9138
Lynxai00 <nydialm2000@gmail.com>
2022-02-01 14:52:30 +0100

    Magic Button Creation

---
+++ magic.js
@@ -0,0 +1,11 @@
+//Event JS
+function myFirstFunction(){
+    console.log("Thank you for your click.");
+}
+function mySecondFunction(){
+    console.log("Thank you for your interest.");
+}
+
+//Select DOM
+var elem = document.getElementsByClassName("my_div");
+console.log(elem);
